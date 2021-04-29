20a8cb9b32d5ccc3b72ee07f4433dfbdf23e301e
michel@digifit.eu <Ha4JZ6ZahugL64dm>
2021-04-29 21:48:35 +0200

    First commit

---
+++ src/views/Info.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="info">
+    <h1>Info page</h1>
+        <CustomComponent msg="Testing2" />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CustomComponent from "@/components/CustomComponent.vue";
+
+export default {
+  name: "Info",
+  components: {
+    CustomComponent,
+  },
+};
+</script>
